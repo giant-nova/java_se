72c29dce54623b9eb9f7e60e7c79e043762fe3d1
Kunal <kunal.datadev.os@gmail.com>
2026-09-06 20:07:16 +0530

    restarted

---
+++ DSA/star_patterns_printing/Rectangular_Star.java
@@ -0,0 +1,25 @@
+/**
+ * Problem Statement: Given an integer N, print the following pattern.
+ *          ****
+ *          ****
+ *          ****
+ *          ****
+ */
+
+
+import java.util.Scanner;
+
+public class Rectangular_Star{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }   
+}
